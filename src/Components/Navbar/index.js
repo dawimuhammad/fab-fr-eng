3e92b479aecb993fe9db229fb088a4226c14f9b5
Haddawi <muhamadhaddawi@gmail.com>
2020-12-04 23:04:37 +0700

    feature/navbar: added navbar root component to app

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import './index.css';
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="navbar-root">
+        {/* searchbar component */}
+        {/* product style options component */}
+        {/* product delivery options component */}
+      </div>
+    )
+  }
+}
+
+export default Navbar;
