04802f3184ecd225159f4c9a590562f711119610
Haddawi <muhamadhaddawi@gmail.com>
2020-12-05 02:56:27 +0700

    feature/product: added fetch products from mockup API

--- src/Components/Contents/Product/index.js
+++ src/Components/Contents/Product/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Grid } from '@material-ui/core'
 import './index.css';
 
-const Product = () => {
+const Product = ({ product }) => {
+  const { name, price, description, furniture_style, delivery_time } = product;
+
   return (
     <div className="product-root">
       <Grid item container direction="row">
         <Grid item container direction="row" justify="space-between">
-          <p className="title">Product Name</p>
-          <p className="price">Price</p>
+          <p className="title">{ name }</p>
+          <p className="price">{ price }</p>
         </Grid>
         
         <Grid item xs={12}>
-          <p className="description">This is life from a disabled lens. Disability Visibility is a podcast hosted by San Francisco night owl Alice Wong featuring conversations on politics, culture, and media with disabled people. If you're interested in disability rights, social justice, and intersectionality, this show is for you. It's time to hear more disabled voices in radio.</p>
+          <p className="description">{ description }</p>
         </Grid>
 
         <Grid item>
@@ -20,7 +22,7 @@ const Product = () => {
         </Grid>
 
         <Grid item container direction="row" justify="flex-end">
-          <p className="delivery-days">Delivery Time</p>
+          <p className="delivery-days">Dikirim dalam { delivery_time } Hari</p>
         </Grid>
       </Grid>
     </div>
--- src/Components/Contents/index.js
+++ src/Components/Contents/index.js
@@ -2,13 +2,26 @@ import { Component } from 'react';
 import { Grid } from '@material-ui/core'
 import Product from './Product';
 
+const fetchProductUrl = 'http://www.mocky.io/v2/5c9105cb330000112b649af8';
+
 class Contents extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      products: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      products: [],
+      furniture_styles: []
     };
+  }
+
+  componentDidMount() {
+
+    fetch(fetchProductUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const { products = [], furniture_styles = [] } = data;
+        this.setState({ products, furniture_styles })
+      });
   }
 
   render() {
@@ -17,7 +30,9 @@ class Contents extends Component {
     return (
       <Grid container direction="row" justify="center" className="content-root">
         {
-          products.map(() => <Product />)
+          (products.length) ? (
+            products.map((product) => <Product product={product}/>)
+          ) : (<></>)
         }
       </Grid>
     )
